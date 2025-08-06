0e11e04f28a033eb4589433575f1c12e0bd6dcda
mera786 <merazalam18011998@gmail.com>
2025-08-06 23:46:51 +0530

    Delete Registration-spring-security-01-main/api-demo directory

--- Registration-spring-security-01-main/api-demo/src/main/java/com/app/service/UserRegistrationService.java
+++
@@ -1,8 +0,0 @@
-package com.app.service;
-
-import com.app.dto.UserRegistrationDto;
-import com.app.payload.APIResponse;
-
-public interface UserRegistrationService {
-    APIResponse<String> registration(UserRegistrationDto dto);
-}
