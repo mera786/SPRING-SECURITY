41777480955b1e9dbf2f70c2ed577789e65421b8
mera786 <merazalam18011998@gmail.com>
2025-08-06 23:47:22 +0530

    Delete complete-registration-registration-login-by-validating-jwt-token-with-spring-security-jwt-03-main/api-demo directory

--- complete-registration-registration-login-by-validating-jwt-token-with-spring-security-jwt-03-main/api-demo/src/main/java/com/app/service/UserRegistrationService.java
+++
@@ -1,9 +0,0 @@
-package com.app.service;
-
-import com.app.dto.UserRegistrationDto;
-import com.app.payload.APIResponse;
-
-public interface UserRegistrationService {
-
-    APIResponse<String> register(UserRegistrationDto dto);
-}
